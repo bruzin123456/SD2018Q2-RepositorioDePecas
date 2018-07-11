e821172299e25a2d372f96aa97d92c00a6c43309
felipebanin <felipe.banin@hotmail.com>
2018-07-11 20:22:16 -0300

    pao de queijo

--- client/commands/AddPartCommand.java
+++ client/commands/AddPartCommand.java
@@ -18,7 +18,19 @@ public abstract class AddPartCommand extends BaseClientCommand {
     @Override
     protected boolean processCommand(Client client, String[] args) {
         if(args.length == 3){
-			addp(args[1], args[2]);
+			RepositoryConnection repositoryConnection = client.getRepositoryConnection();
+            if(repositoryConnection == null){
+                printMessage("Não conectado");
+                return false;
+            }
+            try {
+                int codigoNovaPart = repositoryConnection.partRepository.addPart(args[1], args[2]);
+                System.out.println("Peça adicionada com sucesso. Código: " + String.valueOf(codigoNovaPart));
+            }
+            catch (RemoteException e){
+                e.printStackTrace();
+                printMessage("Erro!!!");
+            }
         }
         else {
             printInvalidArgs();
